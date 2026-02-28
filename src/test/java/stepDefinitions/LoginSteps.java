3edf60c1d26a145f9c1892fdf6ab14877ee84f68
Pearl-Masuku <pthuso@yahoo.com>
2026-02-28 12:25:16 +0200

    created browser factory class

--- src/test/java/stepDefinitions/LoginSteps.java
+++ src/test/java/stepDefinitions/LoginSteps.java
@@ -1,4 +1,31 @@
 package stepDefinitions;
 
+import io.cucumber.java.en.*;
+
 public class LoginSteps {
+
+    @Given("I am on the login page")
+    public void i_am_on_the_login_page() {
+
+    }
+    @When("I enter valid username {}")
+    public void i_enter_valid_username (String username) {
+
+    }
+    @When("I enter valid password {}")
+    public void i_enter_valid_password(String password) {
+
+    }
+
+
+    @When("I click the login button")
+    public void i_click_the_login_button() {
+
+
+    }
+    @Then("I should be redirected to the dashboard")
+    public void i_should_be_redirected_to_the_dashboard() {
+
+    }
+
 }
---
+++ src/test/resources/Features/login.feature
@@ -0,0 +1,13 @@
+Feature: User log in test
+
+  Scenario Outline: Successful login with valid credentials
+    Given I am on the login page
+    When I enter valid username <username>
+    And I enter valid password <password>
+    And I click the login button
+    Then I should be redirected to the dashboard
+
+    Examples:
+      | username         | password  |
+      | pthuso@yahoo.com | Masuku@01 |
+
